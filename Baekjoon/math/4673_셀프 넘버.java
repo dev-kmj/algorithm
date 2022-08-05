4e2a759ee466dcf86a5cecf0e797838c9ade31f3
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-08-05 14:08:28 +0900

    [백준] 4673_셀프넘버

---
+++ Baekjoon/math/4673_셀프 넘버.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean[] numbers = new boolean[10000]; // 셀프 넘버를 구분하기 위한 배열
+
+        for (int i = 1; i < numbers.length; i++) {
+            int n = d(i);
+            if (n < 10001) {
+                numbers[n - 1] = true;    // 셀프넘버가 아니면 true 대입
+            }
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == false) {
+                System.out.println(i + 1);
+            }
+        }
+    }
+
+    static int d(int number) {
+        int sum = number;
+
+        while (number != 0) {
+            sum += (number % 10);   // 일의 자리 수를 sum에 누적
+            number /= 10;    // 자릿수 줄어듬
+        }
+        return sum;
+    }
+}
