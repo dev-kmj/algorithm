12a0724b44607a3d8f97229641a7baafc981f71b
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-08-05 15:03:00 +0900

    Update 1157_단어공부.java

--- Baekjoon/string/1157_단어공부.java
+++ Baekjoon/string/1157_단어공부.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon {
+public class Main {
 
     public static void main(String[] args) throws IOException {
 
