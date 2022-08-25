7d77ef3d7a959c2fbfc56eaebe376a3f34c68cd1
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-08-25 10:15:50 +0900

    [백준] 2217_로프
    그리디 알고리즘 이용, 배열의 정렬(Arrays.sort()) 사용

---
+++ Baekjoon/greedy/2217_로프.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] ropes = new int[n];
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            ropes[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ropes);
+        res = ropes[0] * n;
+
+        for (int i = 1; i < n; i++) {
+            if (ropes[i] * (n - i) > res) {
+                res = ropes[i] * (n - i);
+            }
+        }
+
+        System.out.println(res);
+    }
+}
