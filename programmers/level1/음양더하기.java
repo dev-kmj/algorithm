7b6fd67798ce69378324cec91debc849eeaa1bdb
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-09-16 17:35:58 +0900

    [프로그래머스] 음양더하기

---
+++ programmers/level1/음양더하기.java
@@ -0,0 +1,21 @@
+/*
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
+        return answer;
+    }
+}
+*/
+
+// 삼항연산자 사용 
+// for문은 boolean배열의 길이만큼 반복
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
+        return answer;
+    }
+}
