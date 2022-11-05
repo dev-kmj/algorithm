ecf61908a3d4e8312c6a232ba948199eaf65dbbf
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-11-05 18:08:17 +0900

    [프로그래머스/level1] 3진법뒤집기
    - 진법으로 변환한 결과는 String변수에 담았음

---
+++ programmers/level1/3진법뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String result = "";
+        while(n != 0) {
+            result += n % 3;
+            n /= 3;
+        }
+        answer = Integer.parseInt(result, 3);
+        return answer;
+    }
+}
