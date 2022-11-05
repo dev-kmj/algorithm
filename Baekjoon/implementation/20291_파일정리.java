cd794c051283139b57b115af53e97ec6d3d9a20f
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2022-11-05 17:55:32 +0900

    [백준] 20291_파일정리
    - 구현문제 
    - HashMap과 TreeMap의 차이 기억하기!
    - split("\\.") - 특수문자 기준으로 문자열을 자르고 싶다면 앞에 \\를 넣어줘야 한다.

---
+++ Baekjoon/implementation/20291_파일정리.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Map<String, Integer> map = new TreeMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] temp = br.readLine().split("\\.");
+            if (map.containsKey(temp[1])) {
+                int cnt = map.get(temp[1]);
+                map.replace(temp[1], ++cnt);
+            } else {
+                map.put(temp[1], 1);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
+        }
+    }
+}
