ecd66e348cd3cdcff7d41ebf8093477739ab02d8
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-01-22 15:28:35 +0900

    [프로그래머스/level2] 모음 사전

---
+++ programmers/level2/모음사전.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    static String[] arr;
+    static List<String> list;
+    public int solution(String word) {
+        int answer = 0;
+        
+        list = new ArrayList<>();
+        arr = new String[]{"A", "E", "I", "O", "U"};
+        
+        // 완전탐색 함수 호출  (dfs)
+        dfs(word, "", 0);
+        
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    
+    static void dfs(String word, String str, int depth) {
+        list.add(str);
+        
+        // 5글자
+        if(depth == 5) {
+            return;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            dfs(word, str + arr[i], depth + 1);
+        }
+    }
+}
