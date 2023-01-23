fed1e3910b1fb3692fb91aa0a9d56b9a1e115f4b
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-01-23 16:32:35 +0900

    [백준] 1932_정수 삼각형
    - int[] 배열은 디폴트값이 0이기 떄문에 주어진 범위 0~9999로 0이 겹친다. 탐색 전에 값을 -1로 초기화해주거나 인덱스1부터 탐색

---
+++ Baekjoon/dynamic_programming/1932.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;  // 정수 삼각형 입력 받을 배열 
+    static Integer[][] dp; // 경로 합을 저장할 dp배열 
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        arr = new int[n][n];
+        dp = new Integer[n][n];
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            for (int j = 0; j < i + 1; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = arr[n - 1][i];
+        }
+            System.out.println(find(0, 0));
+    }
+
+    static int find(int depth, int idx) {
+        // 마지막 행일 경우 현재 위치의 dp값 반환
+        if (depth == n -1) return dp[depth][idx];
+
+        // 탐색하지 않았던 값일 경우 다음 행의 양쪽 값 비교
+        if (dp[depth][idx] == null) {
+            dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+        }
+        
+        
+        return dp[depth][idx];
+    }
+
+}
