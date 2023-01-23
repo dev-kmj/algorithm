8c942b5c01659125709ba6ff2c731799847ca43b
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-01-23 10:33:07 +0900

    [백준] 9095_1,2,3더하기
    - dp[4] = dp[3] + dp[2] + dp[1] 이라는 점화식을 찾아내서 사용

---
+++ Baekjoon/dynamic_programming/9095.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        int[] nums = new int[11];   // n은 11보다 작은 양수
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 4;
+        // nums[4] = nums[3] + nums[2] + nums[1] = 7
+        for (int i = 0; i < tc; i++) {
+            int n = sc.nextInt();
+            for (int j = 4; j <= n; j++) {
+                nums[j] = nums[j - 1] + nums[j - 2] + nums[j - 3];
+            }
+            System.out.println(nums[n]);
+        }
+    }
+}
