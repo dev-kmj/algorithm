585a57e117e0ab9533c3de66499bd5a1c0e2bcbd
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-02-02 15:58:23 +0900

    [백준] 14501_퇴사
    - 1일차에 상담을 진행하는 경우 3일이 걸리므로 결과적으로 4일부터 다시 상담을 진행할 수 있다. 최대 이익은 1일차의 상담금액 + 4일부터의 최대 상담 금액이다.
    - 이 원리를 이용하면 뒤쪽 날짜부터 거꾸로 확인해서 계산하는 방법을 사용할 수 있다.
    - n의 개수가 작아서 차이가 없지만 top-down 방식을 사용해야 최종탐색시간이 O(N)이다. (bottom-up은 O(N^2))

---
+++ Baekjoon/dynamic_programming/14501.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] t = new int[15];   // 각 상담 완료 기간
+    static int[] p = new int[15];   // 상담 완료시 받는 금액
+    static int[] dp = new int[16];  // 다이나믹 프로그래밍을 위한 테이블 초기화
+    static int maxValue;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 배열을 뒤에서부터 확인
+        for (int i = n - 1; i >= 0; i--) {
+            int time  = t[i] + i;
+            // 상담이 기간 안에 끝나는 경우
+            if (time <= n) {
+                // 점화식을 사용해서 현재까지 얻을 수 있는 최대 이익 계산
+                dp[i] = Math.max(p[i] + dp[time], maxValue);
+                maxValue = dp[i];
+            }
+            else dp[i] = maxValue;
+        }
+        System.out.println(maxValue);
+    }
+}
