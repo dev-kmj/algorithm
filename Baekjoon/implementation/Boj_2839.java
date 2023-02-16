3c00b9a44ce8df0431c52311f6c80dbf06be8b81
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-02-16 13:16:48 +0900

    [백준] 2839_설탕배달
    구현문제. 
    특히 이런 몫과 나머지에 관련된 문제는 경우의 수를 표처럼 나열해보면서 규칙을 찾아 케이스별로 수식을 만드는 것이 중요함!!

--- Baekjoon/implementation/Boj_2839.java
+++ Baekjoon/implementation/Boj_2839.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class Baekjoon {
+public class Boj_2839 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
