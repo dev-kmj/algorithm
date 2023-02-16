899e7abdba07dc14f99064f87c142afa12223eb3
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-02-16 11:02:04 +0900

    [백준] 2606_바이러스
    - 한 노드의 인접한 노드를 모두 방문하는 bfs 알고리즘 사용

--- Baekjoon/graph/Boj_2606.java
+++ Baekjoon/graph/Boj_2606.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj_2606 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
