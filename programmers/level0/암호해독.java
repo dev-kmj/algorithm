e5e896f69e87f4aedd81c504e2537e4eb0b9c3d0
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-02-20 11:22:16 +0900

    [프로그래머스/level0] 암호 해독

---
+++ programmers/level0/암호해독.java
@@ -0,0 +1,9 @@
+class 암호해독 {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        for(int i=code-1; i<cipher.length(); i+=code){
+            answer += cipher.substring(i, i+1);
+        }
+        return answer;
+    }
+}
