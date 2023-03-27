4438c2e6c0a7959fc7eff4bb9bc4e5c31d919d3f
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-03-27 16:33:10 +0900

    [프로그래머스/level2] 모음 사전
    - dfs/bfs, 완전탐색 등으로 해결 가능 
    -  dfs를 이용해 각 자리마다 알파벳을 하나씩 추가 -> 주어진 알파벳 순서대로 모든 경우 탐색 
    - 가능한 알파벳은 5개이므로 dfs를 5번 재귀호출하면서 각각의 알파벳 추가 (깊이=5가 되면 return)

--- programmers/level2/모음사전.java
+++ programmers/level2/모음사전.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
+class 모음사전전 {
     static String[] arr;
     static List<String> list;
     public int solution(String word) {
