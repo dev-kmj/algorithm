60b89f717ffa34285dc0740f255e8d4d6f137be1
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-06-04 14:58:45 +0900

    [백준] 9012_괄호
    - 스택 
    - '(' 일 때는 push, ')'일 때는 (size = 0이 아닌 경우) pop

---
+++ Baekjoon/string/Boj_9012.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Boj_9012 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+
+            for (int j = 0; j < s.length(); j++) {
+                char ch = s.charAt(j);
+
+                if (ch == '(') stack.push(ch);
+                else {
+                    int size = stack.size();
+                    if (size == 0) {
+                        stack.push(ch);
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            if (stack.isEmpty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+
+            stack.clear();
+        }
+    }
+}
