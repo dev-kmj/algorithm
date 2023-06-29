54dd4b488807a336568e9888de54ae9f90806f5a
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-06-29 22:51:14 +0900

    [프로그래머스/level2] 주시 가격
    - 스택을 사용하지 않고 배열을 순회하는 방법으로도 풀 수 있지만 속도가 많이 느림.
    - 스택은 데이터를 쌓아서 한 번에 처리하기 때문에 시간이 개선됨.

---
+++ programmers/level2/주식가격.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class 주식가 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();   // 스택을 이용한 O(n)순회 
+        
+        for(int i = 0; i < prices.length; i++) {
+            while(!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();   // 가격이 떨어졌다면 숫자 기록 
+                stack.pop();    // 답을 구했기 때문에 스택에서 제거 
+            }
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()) { // 값을 구하지 못한 인덱스 (= 끝까지 가격이 떨어지지 않은 주식)
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+        
+        return answer;
+    }
+}
