6be60759afabea821cd19ce268c3e298cee5c40d
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2023-11-15 22:46:31 +0900

    [프로그래머스/level0] 다음에 올 숫자

---
+++ programmers/level0/다음에올숫자.java
@@ -0,0 +1,13 @@
+class 다음에올숫자 {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        if((common[1] - common[0]) == (common[2] - common[1])) {
+            answer = common[common.length - 1] + (common[1] - common[0]);
+        } else {
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        
+        return answer;
+    }
+}
