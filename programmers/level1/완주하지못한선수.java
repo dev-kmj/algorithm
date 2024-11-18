d286e7b706750b86595fb5d8cc2b4bf94c4ba44a
MoonJeong <74237301+dev-kmj@users.noreply.github.com>
2024-11-18 23:29:56 +0900

    [프로그래머스/level1] 완주하지 못한 선수
    - HashMap의 키를 participant에서 찾으면 이름 개수를 -1한다. 
    - 시간복잡도 : O(N)

---
+++ programmers/level1/완주하지못한선수.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        
+        for(String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) {
+                answer = key;
+                System.out.println(answer);
+                break;
+            }
+        }
+        return answer;
+    }
+}
