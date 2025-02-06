30531f45b10bed8afbfbd6f80246764890cc00c2
Moon <74237301+dev-kmj@users.noreply.github.com>
2025-02-06 20:37:35 +0900

    [프로그래머스/level2] 기능 개발
    - ArrayDeque 사용 
    - 작업 순서는 유지하면서 앞의 기능이 뒤의 기능보다 먼저 완료되어야 하므로 큐를 사용

--- programmers/level2/기능개발.java
+++ programmers/level2/기능개발.java
@@ -2,38 +2,30 @@ import java.util.*;
 
 class 기능개발 {
     public int[] solution(int[] progresses, int[] speeds) {
-
-        List<Integer> list = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < progresses.length; i++) {
-            if ((100 - progresses[i]) % speeds[i] == 0) {
-                q.offer((100 - progresses[i]) / speeds[i]);
-            } else {
-                q.offer((100 - progresses[i]) / speeds[i] + 1);
-            }
+        Queue<Integer> answer = new ArrayDeque<>();
+        
+        int n = progresses.length;
+        
+        // 각 작업의 배포 가능일 계산 
+        int[] daysLeft = new int[n];
+        for (int i = 0; i < n; i++) {
+            daysLeft[i] = (int)Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
-
-        int x = q.poll();
-        int count = 1;
-        while (!q.isEmpty()) {
-            if (x >= q.peek()) {
+        
+        int count = 0;
+        int maxDay = daysLeft[0]; // 현재 배포될 작업 중 가장 늦게 배포 될 작업의 배포가능일 
+        
+        for (int i = 0; i< n; i++) {
+            if (daysLeft[i] <= maxDay) {
                 count++;
-                q.poll();
             } else {
-                list.add(count);
+                answer.add(count);
                 count = 1;
-                x = q.poll();
+                maxDay = daysLeft[i];
             }
         }
-        list.add(count);
-
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-
-
-        return answer;
+        
+        answer.add(count);  //마지막으로 카운트된 작업들을 함께 배포
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
